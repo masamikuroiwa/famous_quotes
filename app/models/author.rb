6878d729cd564445834fd5c8987810110c2367a3
Masami Kuroiwa <m-kuroiwa@froide.co.jp>
2017-04-27 15:13:38 +0900

    add validation to Author.country #3

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,4 @@
 class Author < ApplicationRecord
   validates :name, presence: true, length: { maximum: 255 }
+  validates :country, length: { maximum: 255 }
 end
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,18 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
-  let(:author) { FactoryGirl.build(:author) }
-  specify "妥当なオブジェクト" do
-    expect(author).to be_valid
-  end
-  specify "nameは空であってはならない" do
-    author[:name] = ''
-    expect(author).not_to be_valid
-    expect(author.errors[:name]).to be_present
-  end
-  specify "nameは255文字以内" do
-    author[:name] = 'ア' * 256
-    expect(author).not_to be_valid
-    expect(author.errors[:name]).to be_present
+  describe '#valid?' do
+    let(:author) { FactoryGirl.build(:author) }
+    example "妥当なオブジェクト" do
+      expect(author).to be_valid
+    end
+    example "nameは空であってはならない" do
+      author[:name] = ''
+      expect(author).not_to be_valid
+      expect(author.errors[:name]).to be_present
+    end
+    example "nameは255文字以内" do
+      author[:name] = 'ア' * 256
+      expect(author).not_to be_valid
+      expect(author.errors[:name]).to be_present
+    end
+    example "countryは255文字以内" do
+      author[:country] = 'ア' * 256
+      expect(author).not_to be_valid
+      expect(author.errors[:country]).to be_present
+    end
   end
 end
