e0f40004ef6c83482974919ed2da50c7debd3335
Masami Kuroiwa <m-kuroiwa@froide.co.jp>
2017-05-08 14:21:15 +0900

    scaffold monban #5

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root to: 'authors#index'
+  resource :session, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create]
   resources :authors
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
