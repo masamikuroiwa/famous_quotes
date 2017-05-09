610d4cee828f3e62742dbcd098f8464b26ad98e2
Masami Kuroiwa <m-kuroiwa@froide.co.jp>
2017-05-09 13:17:11 +0900

    add redirect to sign_in when not signed_in #5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
   include Monban::ControllerHelpers
   protect_from_forgery with: :exception
+
+  before_filter :require_login
 end
---
+++ config/initializers/monban.rb
@@ -0,0 +1,4 @@
+Monban.configure do |config|
+  # noticeを非設定に
+  config.sign_in_notice = nil
+end
